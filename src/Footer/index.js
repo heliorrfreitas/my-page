ba226e4c6afda46b821e3017ef1ef1b983ff9bf9
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-08-25 22:34:17 -0500

    Delete files I am not going to use. Add files for footer, header, and for the about me page.

---
+++ src/Footer/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './style.css'
+
+export default function Footer() {
+    return (<div className="footer">This is the footer. Add a impactful phrase here!</div>)
+}
---
+++ src/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css'
+
+export default function Header() {
+    return (<div className="header">This is the header</div>)
+}
