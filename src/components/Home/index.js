e19efd1b07c4c8f6b262103dc097817c893f78a9
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-09-14 22:30:07 -0500

    Create structure of the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,24 @@
 import React from "react"
+import { HashRouter, Link } from 'react-router-dom'
+import "./styles.css"
+
 
 function Home() {
     return (
-        <div>
-            HOME
+        <div className="home">
+            <h2>Welcome, I am Helio Rui Ramos Freitas!</h2>
+            <p>You can check out somethings I have built on my free time or things that I did to study going to my projects page. 
+                The About me page has a little bit more about me. </p>
+
+            <HashRouter basename="/">                
+                <div className="other-pages">
+                    <Link to="/projects">Projects</Link>                    
+                </div>
+                <div className="other-pages">
+                    <Link to="/about-me">About Me</Link>
+                </div>
+            </HashRouter>
+                
         </div>
     )
 }
