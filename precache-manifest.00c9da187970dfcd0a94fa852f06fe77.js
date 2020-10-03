3ea25690a726593bd576cb42d8da9a8a0e058e3a
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-10-02 22:50:19 -0500

    Updates

---
+++ precache-manifest.00c9da187970dfcd0a94fa852f06fe77.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72e6e4bc96467e827abaa1b3e0b77d3",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "8acd40db7dfd1dc83836",
+    "url": "/my-page/static/css/main.625c4a14.chunk.css"
+  },
+  {
+    "revision": "b6dcb640851baf93f202",
+    "url": "/my-page/static/js/2.3a706e81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-page/static/js/2.3a706e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8acd40db7dfd1dc83836",
+    "url": "/my-page/static/js/main.c291c473.chunk.js"
+  },
+  {
+    "revision": "24a74e670725a91afbb1",
+    "url": "/my-page/static/js/runtime-main.9afb9fb7.js"
+  },
+  {
+    "revision": "1acb1a2299320cbf24d5d921bd0300ed",
+    "url": "/my-page/static/media/base-converter.1acb1a22.png"
+  },
+  {
+    "revision": "1670518eb17af8f3f5c31dd5d15dde0a",
+    "url": "/my-page/static/media/be-the-hero.1670518e.png"
+  },
+  {
+    "revision": "9b3d45bf9d01e69d139d8c57c0e09123",
+    "url": "/my-page/static/media/border-radius-generator.9b3d45bf.png"
+  },
+  {
+    "revision": "4ff2da976c3990ff89e7fe001281df2a",
+    "url": "/my-page/static/media/ecoleta.4ff2da97.png"
+  },
+  {
+    "revision": "f981fa5e0f8e378692b23a1c6ae6543a",
+    "url": "/my-page/static/media/meme-generator.f981fa5e.png"
+  },
+  {
+    "revision": "0705bfae361deae4dcece8329b3d0c95",
+    "url": "/my-page/static/media/simple-drawing-app.0705bfae.png"
+  },
+  {
+    "revision": "925bd579910363c62d46377f8b34c810",
+    "url": "/my-page/static/media/space-invaders.925bd579.png"
+  }
+]);
